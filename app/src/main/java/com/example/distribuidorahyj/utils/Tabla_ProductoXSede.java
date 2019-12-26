81fe06e3ed8db3c51a524f8a6e495d2530a30d35
mac <yackson150@gmail.com>
2019-12-26 10:37:32 -0500

    Se ingresa la tabla Sede y ProductoXSede

---
+++ app/src/main/java/com/example/distribuidorahyj/utils/Tabla_ProductoXSede.java
@@ -0,0 +1,14 @@
+package com.example.distribuidorahyj.utils;
+
+public class Tabla_ProductoXSede {
+
+    public static final String TABLA_PRODUCTOXSEDE="ProductoXSede";
+    public static final String CAMPO_CODIGOPRODUCTO="codigoProducto";
+    public static final String CAMPO_CODIGOSEDE="codigoSede";
+    public static final String CAMPO_ACTIVO="activo";
+
+
+    public static final String CREAR_TABLA_RODUCTOXSEDE="CREATE TABLE "+ TABLA_PRODUCTOXSEDE+" ("+CAMPO_CODIGOPRODUCTO+" INTEGER primary key AUTOIMCREMENTABLE, "+CAMPO_CODIGOSEDE+" STRING, "+CAMPO_ACTIVO+" STRING)";
+
+}
+
